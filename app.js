3e1900fe47dd80a6afae18478a638392702c76a6
David Ferrara <davidferrara@live.com>
2022-07-01 10:04:20 -0400

    Deleted Register controller

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const loginRouter = require('./controllers/login');
-const registerRouter = require('./controllers/register');
 const usersRouter = require('./controllers/users');
 const productsRouter = require('./controllers/products');
 const middleware = require('./utils/middleware');
@@ -17,7 +16,6 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 app.use('/api/login', loginRouter);
-// app.use('/api/register', registerRouter);
 app.use('/api/users', middleware.userExtractor, usersRouter);
 app.use('/api/products', middleware.userExtractor, productsRouter);
 
